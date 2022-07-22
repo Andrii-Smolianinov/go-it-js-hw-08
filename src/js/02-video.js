9c070630e01bd9a00f24cd94fa5514f2cfc7ebad
Andrii-Smolianinov <andr380v@gmail.com>
2022-07-22 18:24:05 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,33 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const vimeoPlayer = new Player(iframe);
+const TIME_CODE = 'videoplayer-current-time';
 
-vimeoPlayer.on('play', function() {
-    console.log('played the video!');
-});
 
+const getLocalTimeOptions = () => {
+  let initialTime = 0;
+  
+  const localDataTime = localStorage.getItem(TIME_CODE);
+  if (localDataTime) {
+    initialTime = parseFloat(localDataTime);
+  }
+  return initialTime;
+};
+
+const initialTime = getLocalTimeOptions();
+
+vimeoPlayer.setCurrentTime(initialTime);
+
+vimeoPlayer.on('timeupdate', throttle(onVideoPlay, 1000));
+vimeoPlayer.on('ended', onVideoEnded);
+
+function onVideoPlay(event) {
+  if (event.duration - event.seconds < 1) {
+    return;
+  }
+  localStorage.setItem(TIME_CODE, event.seconds);
+}
+
+function onVideoEnded(event) {
+  localStorage.removeItem(TIME_CODE);
+}
